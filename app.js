4fff080c12ae9ee7a6fe4442dea05a20a7a7d0c5
Jonathan Rubin Yaniv <jorub@microsoft.com>
2017-06-21 00:33:38 +0300

    Added commander and support for custom config and output path

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
+let program = require('commander');
 let fs = require('fs');
 let xmlGenerator = require('js2xmlparser');
 let uuid = require('uuid/v4');
 let domparser = require('./domparser');
 
-const config = JSON.parse(fs.readFileSync('./hm.config.json', 'utf8'));
-validateConfig(config);
+program
+	.version('1.0.0')
+	.option('-c, --config [path]', 'config file path')
+	.option('-o, --output [path]', 'output file path (health model definition)')
+	.option('-u, --update-test-xmls', 'update the test-list xml files with the new GUIDs')
+	.parse(process.argv);
 
-const hmTree = createHealthModelTree(config);
-const xml = xmlGenerator.parse("HealthModelConfiguration", hmTree)
-	.replace(/'/g, '"')
-	.replace('<?xml version="1.0"?>', '<?xml version="1.0" encoding="utf-8"?>');
-
-console.log(xml);
-
-config.nodes.forEach(function (node) {
-	domparser.updateTestFile(node.monitorIds, node.testListPath);
-});
+const config = JSON.parse(fs.readFileSync(program.config, 'utf8'));
+generateHealthModel(config);
 
 process.exit(0);
+
+// End of program here
+
+function generateHealthModel(config) {
+	const hmTree = createHealthModelTree(config);
+	const xml = xmlGenerator.parse("HealthModelConfiguration", hmTree)
+		.replace(/'/g, '"')
+		.replace('<?xml version="1.0"?>', '<?xml version="1.0" encoding="utf-8"?>');
+
+	fs.writeFileSync(program.output, xml);
+	config.nodes.forEach(function (node) {
+		domparser.updateTestFile(node.monitorIds, node.testListPath);
+	});
+
+}
 
 function createHealthModelTree(config) {
 	const nodes = config.nodes;
@@ -71,8 +83,4 @@ function createHealthModelTree(config) {
 
 function getMetricName(testNamePrefix, testNameSuffix) {
 	return `${testNamePrefix}${testNameSuffix} Test Failure Count`
-}
-
-function validateConfig(config) {
-
 }
